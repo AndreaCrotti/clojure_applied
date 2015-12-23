a3a43c1a33c7cfde76971b2c14d48e06d182485f
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-12-23 18:14:13 +0000

    upgrade version

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0-RC4"]
-
-                 [org.clojure/test.check "0.8.2"]
-])
+                 [org.clojure/test.check "0.9.0"]])
